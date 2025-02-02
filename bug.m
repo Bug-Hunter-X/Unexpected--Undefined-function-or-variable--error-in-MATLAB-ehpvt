2f5db2d5cc72aa3356baf39aca1430c1bfb256dd
BugHunter-X <askzenai@gmail.com>
2025-02-02 03:30:05 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,14 @@
+```matlab
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+  else
+    result = someOtherValue; 
+  end
+end
+
+% Example usage
+input = [1, 2, 3];
+result = myFunction(input);
+```
